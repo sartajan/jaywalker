2ca921cb4b2af4e0630768e16fbe94d2988e2ba8
sartajan <tajani.sunny@gmail.com>
2020-07-21 22:16:35 -0400

    Update MainActivity.java
    Commented

--- app/src/main/java/com/example/sunny/thejaywalker/MainActivity.java
+++ app/src/main/java/com/example/sunny/thejaywalker/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Layout inflater  
         LayoutInflater l=getLayoutInflater();
         View v=l.inflate(R.layout.activity_main, null);
     }
